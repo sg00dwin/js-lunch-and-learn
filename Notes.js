2707d85d1bb827480984b56b9deb36ce8ab62e7a
Steve Goodwin <sgoodwin@redhat.com>
2017-03-30 12:24:34 -0400

    add my notes

--- Notes.js
+++ Notes.js
@@ -56,3 +56,29 @@ switch(name) {
 
 // Errors, Try, Catch, Finally & Throw
 throw new Error('dang it broke');
+
+
+// Types and Primitives
+
+// To understand this article, you’ll need to understand the difference between primitive and non-primitive values in JavaScript. There are 5 primitive types, which are associated with the various primitive values.
+
+Null: The value null.
+Undefined: The value undefined.
+Number: All numbers, such as 0 and 3.14. Also NaN, and Infinity.
+Boolean: The values true and false.
+String: All strings, such as "foo" and "".
+
+// All other values are non-primitive, including arrays, functions, and plain old objects. For completeness, here are the results of the typeof operator, applied to these values:
+
+typeof null; // "object"
+typeof undefined; // "undefined"
+typeof 0; // "number" (`typeof NaN` is also "number")
+typeof true; // "boolean"
+typeof "foo"; // "string"
+typeof {}; // "object"
+typeof function () {}; // "function"
+typeof []; // "object"
+
+
+// Scoping and hoisting
+// http://www.adequatelygood.com/JavaScript-Scoping-and-Hoisting.html
