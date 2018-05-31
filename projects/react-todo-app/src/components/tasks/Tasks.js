b1554307b4fa41d2c013e20c2c6dec189cf33a85
sg00dwin <sgoodwin@redhat.com>
2018-05-31 15:24:49 -0400

    Begin breaking apart html into components

---
+++ projects/react-todo-app/src/components/tasks/Tasks.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Tasks = () => (
+  <aside>
+  <h3>App functionality to build</h3>
+  <ol>
+  <li>Add item</li>
+  <li>Remove item</li>
+  <li>Toggle item completed (green && strikethrough)</li>
+  <li>Toggle item not completed</li>
+  <li>Toggle star and list those items first (blue star)</li>
+  <li>Track # of items not completed</li>
+  <li>Track # of items completed</li>
+  <li>View only not completed items</li>
+  <li>View only completed items</li>
+  <li>Clear completed items</li>
+  <li>Clear all items</li>
+  </ol>
+  </aside>
+);
+
+export default Tasks;
